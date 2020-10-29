36f0a5b6c494bb6c3c26b6d91c7b4c9c26e8f3be
mehrab-haque <mehrab.haque.0001@gmail.com>
2020-10-29 15:51:37 +0600

    fixed a bug

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,11 @@ const axios=require('axios');
 
 admin.initializeApp();
 
-const SQL_LOG_URL='http://bkashwebhookendpoint-env.eba-7pkphtwu.ap-south-1.elasticbeanstalk.com/user_serial'
+const SQL_LOG_URL='http://buetedu1-env.eba-zxgyn7t7.ap-south-1.elasticbeanstalk.com/author/addNew'
 
 exports.registerUser = functions.auth.user().onCreate((user) => {
   if('displayName' in user && user.displayName!==null){
-    console.log(user)
+    //console.log(user)
     var userData={
       name:user.displayName,
       timestamp:Date.now()
@@ -16,12 +16,15 @@ exports.registerUser = functions.auth.user().onCreate((user) => {
     if('photoURL' in user && user.photoURL!==null)
       userData['image']=user.photoURL
     return admin.firestore().collection('profile').doc(user.uid).set(userData).then(()=>{
-      return axios.post(SQL_LOG_URL,{
+      var postData={
         author_id:user.uid,
         name:user.displayName,
         contact:user.email,
         image:user.photoURL
-      }).then(res=>{
+      }
+      return axios.post(SQL_LOG_URL,postData).then(res=>{
+        console.log(postData)
+        console.log(res.data)
         return console.log('user added, uid - '+user.uid)
       }).catch(err=>{
         return console.log(err)
