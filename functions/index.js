12844aa6fd648ce5359e3f1a7f45513e2a92d1de
mehrab-haque <mehrab.haque.0001@gmail.com>
2020-10-29 15:31:56 +0600

    done logging author registration to sql stack endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const axios=require('axios');
 
 admin.initializeApp();
 
+const SQL_LOG_URL='http://bkashwebhookendpoint-env.eba-7pkphtwu.ap-south-1.elasticbeanstalk.com/user_serial'
+
 exports.registerUser = functions.auth.user().onCreate((user) => {
   if('displayName' in user && user.displayName!==null){
+    console.log(user)
     var userData={
       name:user.displayName,
       timestamp:Date.now()
@@ -12,7 +16,16 @@ exports.registerUser = functions.auth.user().onCreate((user) => {
     if('photoURL' in user && user.photoURL!==null)
       userData['image']=user.photoURL
     return admin.firestore().collection('profile').doc(user.uid).set(userData).then(()=>{
-      return console.log('use added, uid->'+user.uid)
+      return axios.post(SQL_LOG_URL,{
+        author_id:user.uid,
+        name:user.displayName,
+        contact:user.email,
+        image:user.photoURL
+      }).then(res=>{
+        return console.log('user added, uid - '+user.uid)
+      }).catch(err=>{
+        return console.log(err)
+      })
     }).catch(err=>{
       return console.log(err)
     })
